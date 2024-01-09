404b71dcd6b01057cbba73bff23ac1e753789976
ldingPU <146761717+ldingPU@users.noreply.github.com>
2024-01-09 11:57:34 -0500

    Update ssmain.py\

--- ssmain.py
+++ ssmain.py
@@ -58,26 +58,25 @@ for i in range(len(Xss)):
     Asys = Asys.subs([(x[i], Xss[i])])
 
 Asys = np.array(Asys).astype(np.float64)
-Asys = Asys(2:, )
 eigvals, eigenvectors = np.linalg.eig(Asys)
 lefteigenvectors = np.linalg.inv(eigenvectors)
 pmatrix = np.multiply(eigenvectors,np.transpose(lefteigenvectors))
 
 fig = px.imshow(abs(pmatrix),
                 labels=dict(x="modes", y="state variables"),
-                x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33],
-                y = ['P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
+                x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34],
+                y = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
                      'theta2','epsilonL2','wf2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
                      'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
                 )
 fig.update_layout(height=800)
 st.plotly_chart(fig, height=800, theme="streamlit")
 
-mode = range(1,len(eigvals))
+mode = range(1,len(eigvals)+1)
 realpart = eigvals.real
 imagpart = eigvals.imag
 frequency = eigvals.imag/2/math.pi
-dampingratio = -eigvals.real/eigvals.real
+dampingratio = -eigvals.real/np.sqrt(realpart.sqaure+imagpart.sqaure)
 list_of_tuples = list(zip(mode, realpart, imagpart, frequency, dampingratio)) 
 df = pd.DataFrame(list_of_tuples,
                  columns = ["mode", "real", "image", "Frequency(Hz)", "damping ratio"])
