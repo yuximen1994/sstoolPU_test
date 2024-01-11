95d4e5507b7e0ce231d4832cf74c69f91bc52738
ldingPU <146761717+ldingPU@users.noreply.github.com>
2024-01-11 00:03:41 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -71,7 +71,7 @@ labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
 sizes = [15, 30, 45, 10]
 explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 figpie, axpie = plt.subplots()
-aspie.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+axpie.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
           shadow=True, startangle=90)
 axpie.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
