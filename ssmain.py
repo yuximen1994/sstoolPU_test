96b6b897e50a684842f6cf23e842d6afa91f7946
ldingPU <146761717+ldingPU@users.noreply.github.com>
2024-01-11 01:31:54 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -56,7 +56,7 @@ pmatrixabs = abs(pmatrix)
 stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
                      'theta2','epsilonL2','wf2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
                      'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
-modeNames = [ 'mode{}'.format(i) for i in range(1,numeigs)]
+modeNames = np.array(['mode{}'.format(i) for i in range(1,numeigs)])
 
 # plot participation factor map
 fig = px.imshow(pmatrixabs,
