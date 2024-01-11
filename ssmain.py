b2ae8329130b711c3c8490a7a8228021e520ef2a
ldingPU <146761717+ldingPU@users.noreply.github.com>
2024-01-11 00:06:13 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -57,8 +57,7 @@ stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','
 fig = px.imshow(abs(pmatrix),
                 labels=dict(x="modes", y="state variables"),
                 x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34],
-                y = 
-                )
+                y = stateVariableNames)
 fig.update_layout(height=800)
 st.plotly_chart(fig, height=800, theme="streamlit")
 
