ed6fa77623a473b83f7ff88e4b9ec40b0a16e2fd
ldingPU <146761717+ldingPU@users.noreply.github.com>
2024-01-11 01:13:53 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -79,11 +79,11 @@ axpie.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 st.pyplot(figpie)
 
 
-df = pd.DataFrame(pmatrixabs, columns=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34'])
+df = pd.DataFrame(pmatrixabs, columns=['col1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34'])
 df.insert(0, "statevariables", stateVariableNames, True)
 # Represent state variables with a relatively larger participation factor
-df.loc[df['1'] >= 0.1, 'statevariables'] = 'Other state variables'
-fig = px.pie(df, values='1', names=statevariables, title='Population of European continent')
+df.loc[df['col1'] >= 0.1, 'statevariables'] = 'Other state variables'
+fig = px.pie(df, values='col1', names=statevariables, title='Population of European continent')
 st.plotly_chart(fig, height=800, theme="streamlit")
 
 # Check if the input is a number and within the desired range
