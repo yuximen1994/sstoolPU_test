17a9310d66031d3fbc177bf0b3ef03e60e1a686a
Lizhi Ding <lizhi.ding2019@gmail.com>
2024-01-12 17:12:54 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -40,34 +40,35 @@ if (sidebar1,sidebar2) == ('GFM_Droop','GFM_Droop'):
           -0.1388,0.0386,-0.1930,0.5132,-0.1407,0.9993,0.5147,0.1411,
            0.1452,-0.0386,0.0844,-0.0002,0.5228,-0.1388,0.0386,-0.1930,
            0.5132,-0.1407,1.0249,-0.2814,0.5127,-0.1407,0.5127,-0.1407]
-    st.write(Xss)
+    stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
+                          'theta2','epsilonL2','wf2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
+                        'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
 elif (sidebar1,sidebar2) == ('GFM_Droop','GFL'):
     st.write('You selected GFM_Droop and GFL.')
+    sysData = case_3bus_droop_gfl()
+    x, xdot = lsm_sys_droop_gfl(sysData)
+    Xss = [0.0000,0.5147,0.1411,0.1452,-0.0386,0.0844,-0.0002,0.5228,
+           -0.1388,0.0386,-0.1930,0.5132,-0.1407,0.0000,0.0000,0.9993,
+           0.5147,0.1411,0.1452,-0.0386,0.0844,-0.0002,0.5228,-0.1388,
+           0.0386,-0.1930,0.5132,-0.1407,1.0249,-0.2814,0.5127,-0.1407,
+           0.5127,-0.1407]
+    stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
+                          'theta2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
+                          'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
 elif (sidebar1,sidebar2) == ('GFL','GFM_Droop'):
     st.write('You selected GFL and GFM_Droop.')
 else:
     st.write("The selected combination is not supported.")
-
-sysData = case_3bus_droop_gfl()
-x, xdot = lsm_sys_droop_gfl(sysData)
-Xss = [0.0000,0.5147,0.1411,0.1452,-0.0386,0.0844,-0.0002,0.5228,
-       -0.1388,0.0386,-0.1930,0.5132,-0.1407,0.0000,0.0000,0.9993,
-       0.5147,0.1411,0.1452,-0.0386,0.0844,-0.0002,0.5228,-0.1388,
-       0.0386,-0.1930,0.5132,-0.1407,1.0249,-0.2814,0.5127,-0.1407,
-       0.5127,-0.1407]
 Asys = xdot.jacobian(x)
 for i in range(len(Xss)):
     Asys = Asys.subs([(x[i], Xss[i])])
-
 Asys = np.array(Asys).astype(np.float64)
 eigvals, eigenvectors = np.linalg.eig(Asys)
 numeigs = len(eigvals)
 lefteigenvectors = np.linalg.inv(eigenvectors)
 pmatrix = np.multiply(eigenvectors,np.transpose(lefteigenvectors))
 pmatrixabs = abs(pmatrix)
-stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
-                     'theta2','epsilonL2','wf2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
-                     'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
+
 modeNames = ['mode{}'.format(i) for i in range(1,numeigs+1)]
 # plot participation factor map
 figheatmap = px.imshow(pmatrixabs,
