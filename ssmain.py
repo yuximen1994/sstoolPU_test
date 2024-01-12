ee4ae3c7ed040d4dfdd534cf489a8ffe9585c3b8
Lizhi Ding <lizhi.ding2019@gmail.com>
2024-01-12 17:27:52 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -41,7 +41,7 @@ if (sidebar1,sidebar2) == ('GFM_Droop','GFM_Droop'):
            0.1452,-0.0386,0.0844,-0.0002,0.5228,-0.1388,0.0386,-0.1930,
            0.5132,-0.1407,1.0249,-0.2814,0.5127,-0.1407,0.5127,-0.1407]
     stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
-                          'theta2','epsilonL2','wf2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
+                          'theta2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
                           'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
 elif (sidebar1,sidebar2) == ('GFM_Droop','GFL'):
     st.write('You selected GFM_Droop and GFL.')
@@ -53,7 +53,7 @@ elif (sidebar1,sidebar2) == ('GFM_Droop','GFL'):
            0.0386,-0.1930,0.5132,-0.1407,1.0249,-0.2814,0.5127,-0.1407,
            0.5127,-0.1407]
     stateVariableNames = ['theta1','P01','Qo1','phid1','phiq1','gammad1','gammaq1','iid1','iiq1','vcd1','vcq1','iod1','ioq1',
-                          'theta2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
+                          'theta2','epsilonL2','wf2','P02','Qo2','phid2','phiq2','gammad2','gammaq2','iid2','iiq2','vcd2','vcq2','iod2','ioq2',
                           'ibranchD1','ibranchQ1','ibranchD2','ibranchQ2','iloadD','iloadQ']
 elif (sidebar1,sidebar2) == ('GFL','GFM_Droop'):
     st.write('You selected GFL and GFM_Droop.')
@@ -75,7 +75,7 @@ modeNames = ['mode{}'.format(i) for i in range(1,numeigs+1)]
 # plot participation factor map
 figheatmap = px.imshow(pmatrixabs,
                        labels=dict(x="modes", y="state variables"),
-                       x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34],
+                       x = list(range(1,numeigs+1)),
                        y = stateVariableNames)
 figheatmap.update_layout(height=800)
 
