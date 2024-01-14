08be57628706b474cbe142844a85b987d9f8328a
Yuxi <155663065+yuximen1994@users.noreply.github.com>
2024-01-13 20:32:46 -0500

    Update ssmain.py

--- ssmain.py
+++ ssmain.py
@@ -186,11 +186,11 @@ styled_html = f"""
         text-align: center;
         white-space: nowrap; 
     }}
-    td {{
-        border: 1px solid black;
-        padding: 8px;
-        text-align: left;
-    }}
+#    td {{
+#        border: 1px solid black;
+#        padding: 8px;
+#        text-align: left;
+#    }}
 
 </style>
 {html}
